43a5445dde8d78d33fbd5eb192856892291d1605
verniy <orekueito2811@gmail.com>
2023-09-17 07:06:24 +0900

    feat: type convert method from Interger to uint

--- integer.go
+++ integer.go
@@ -32,6 +32,22 @@ func (i *Integer[T]) Int64() int64 {
 	return int64(i.value())
 }
 
+func (i *Integer[T]) UInt8() uint8 {
+	return uint8(i.value())
+}
+
+func (i *Integer[T]) UInt16() uint16 {
+	return uint16(i.value())
+}
+
+func (i *Integer[T]) UInt32() uint32 {
+	return uint32(i.value())
+}
+
+func (i *Integer[T]) UInt64() uint64 {
+	return uint64(i.value())
+}
+
 func (i *Integer[T]) Str() *Str {
 	return fromStringer(i)
 }
--- integer_test.go
+++ integer_test.go
@@ -11,3 +11,12 @@ func ExampleInt() {
 	// Output:
 	// i: 6
 }
+
+func ExampleInt_UInt8() {
+	i := NewInteger(9223372036854775807)
+	ui8 := i.UInt8()
+	fmt.Printf("ui8: %v\n", ui8)
+
+	// Output:
+	// ui8: 255
+}
