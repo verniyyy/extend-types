ce36efd2d74c6fe6bfa8a5d08c0e146897ce6186
verniy <orekueito2811@gmail.com>
2023-09-17 05:52:45 +0900

    refactor: unuse basic interface

--- basic.go
+++ basic.go
@@ -6,65 +6,47 @@ import (
 	"github.com/verniyyy/extend-types/lib"
 )
 
-type basic[T comparable] interface {
-	fmt.Stringer
-	Comparator[T]
-
-	Read() T
-	Validate() error
-	Ptr() *T
-
-	// Print with label.
-	// if label="example" then output:
-	// example: ${b.v}
-	Print(label string)
-
-	// CoreTypeName returns the type parameter name of a
-	// basic type as a string.
-	CoreTypeName() string
-}
-
-type b[T comparable] struct {
+type basic[T comparable] struct {
 	v T
 }
 
 func newBasic[T comparable](v T) basic[T] {
-	return &b[T]{
+	return basic[T]{
 		v: v,
 	}
 }
 
-func (b *b[T]) String() string {
+func (b *basic[T]) String() string {
 	return fmt.Sprint(b.v)
 }
 
-func (b *b[T]) Read() T {
+func (b *basic[T]) Read() T {
 	return b.v
 }
 
-func (b *b[T]) Ptr() *T {
+func (b *basic[T]) Ptr() *T {
 	return &b.v
 }
 
-func (b *b[T]) Equal(v T) bool {
+func (b *basic[T]) Equal(v T) bool {
 	return b.v == v
 }
 
-func (b *b[T]) DeepEqual(v T) bool {
+func (b *basic[T]) DeepEqual(v T) bool {
 	return lib.DeepEqual(b.v, v)
 }
 
 // Print with label.
 // if label="example" then output:
 // example: ${b.v}
-func (b *b[T]) Print(label string) {
+func (b *basic[T]) Print(label string) {
 	fmt.Printf("%s: %v\n", label, b.v)
 }
 
-func (b *b[T]) CoreTypeName() string {
+func (b *basic[T]) CoreTypeName() string {
 	return lib.TypeName(b.v)
 }
 
-func (b *b[T]) Validate() error {
+func (b *basic[T]) Validate() error {
 	return nil
 }
