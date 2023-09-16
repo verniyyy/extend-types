3c2c6d7c1e28b857a0366676f81c11e213082e11
verniy <orekueito2811@gmail.com>
2023-09-17 07:47:11 +0900

    feat: type convert method from Float to Integer

--- float.go
+++ float.go
@@ -24,6 +24,42 @@ func (f *Float[T]) Float64() float64 {
 	return float64(f.value())
 }
 
+func (f *Float[T]) Int() int {
+	return int(f.value())
+}
+
+func (f *Float[T]) Int8() int8 {
+	return int8(f.value())
+}
+
+func (f *Float[T]) Int16() int16 {
+	return int16(f.value())
+}
+
+func (f *Float[T]) Int32() int32 {
+	return int32(f.value())
+}
+
+func (f *Float[T]) Int64() int64 {
+	return int64(f.value())
+}
+
+func (f *Float[T]) UInt8() uint8 {
+	return uint8(f.value())
+}
+
+func (f *Float[T]) UInt16() uint16 {
+	return uint16(f.value())
+}
+
+func (f *Float[T]) UInt32() uint32 {
+	return uint32(f.value())
+}
+
+func (f *Float[T]) UInt64() uint64 {
+	return uint64(f.value())
+}
+
 func (f *Float[T]) Ceil() T {
 	return T(math.Ceil(f.Float64()))
 }
